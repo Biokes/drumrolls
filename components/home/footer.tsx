997fdf0ce83ef4593e74d84277b494141c2bade9
Biokes <106444765+Biokes@users.noreply.github.com>
2024-11-15 20:01:27 +0100

    update footer

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -2,7 +2,7 @@ import home from  '@/public/home.svg'
 import Image from 'next/image'
 export default function Footer(){
     return (
-        <div className={'pt-[50px] flex flex-col lg:flex-row gap-[30px]'}>
+        <div className={'pt-[50px] flex flex-col lg:flex-row gap-[30px] sm:pl-[20%] lg:pl-0'}>
             <div className={'pl-[10px] w-full md:w-[400px]'}>
                 <p className={'text-[18px] md:text-[20px] font-bold'}>Contact Information</p>
                 <div className={'flex flex-col gap-[13px] pt-[20px] pl-[20px]'}>
@@ -27,10 +27,10 @@ export default function Footer(){
             </div>
 
             <div className={'flex flex-col lg:flex-row gap-[15px] pl-[15px] md:pl-0'}>
-                <input type="text" placeholder={'Full Name'} className={'w-[220px] h-[50px] p-[10px_15px] rounded-md border-1 outline-none'}/>
-                <input type="email" placeholder={'Email'} className={'w-[220px] h-[50px] p-[10px_15px] rounded-md border-1 outline-none'}/>
-                <input type="text" placeholder={'Phone Number'} className={'w-[220px] h-[50px] p-[10px_15px] rounded-md border-1 outline-none'}/>
-                <input type="text"/>
+                <input type="text" placeholder={'Full Name'} className={'w-[220px] sm:w-[50%] h-[50px] p-[10px_15px] rounded-md border-1 outline-none'}/>
+                <input type="email" placeholder={'Email'} className={'w-[220px] sm:w-[50%] h-[50px] p-[10px_15px] rounded-md border-1 outline-none'}/>
+                <input type="text" placeholder={'Phone Number'} className={'w-[220px] sm:w-[50%] h-[50px] p-[10px_15px] rounded-md border-1 outline-none'}/>
+                <textarea className="w-[220px] sm:w-[50%] h-[100px] p-2 border rounded-md resize-none outline-none" placeholder="Enter your text here"/>
                 <button className={'text-sm font-bold h-[40px] text-center bg-[#FE6A28] text-white w-[150px] rounded-md'}>Send Message</button>
             </div>
         </div>
