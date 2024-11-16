3b6ef95676cea1320adccdfd29b6f558b0e471e1
Yisaterese <teresejosephyisa@gmail.com>
2024-11-16 08:42:13 +0100

    initialise the map in the map component and styled it

--- components/home/map.tsx
+++ components/home/map.tsx
@@ -1,6 +1,30 @@
-export default function GMap(){
-    return (
-        <>
-        </>
-    )
+'use client';
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function GMap() {
+    useEffect(() => {
+        const mapContainer = document.getElementById('map');
+
+        if (mapContainer && !(mapContainer as unknown as { _leaflet_id: string })._leaflet_id) {
+            const map = L.map(mapContainer).setView([40.7128, -74.0060], 12);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            const marker = L.marker([40.7128, -74.0060]).addTo(map);
+            marker.bindPopup("<div style='text-align: center;'><b style='color: #FE6A28;'>HERISTAYS</b><br>Business Park Theale C1, Center Berkshire RS1 5A1 England</div>").openPopup();
+        }
+
+        return () => {
+            if (mapContainer && (mapContainer as unknown as { _leaflet_id: string })._leaflet_id) {
+                const mapInstance = L.map(mapContainer);
+                mapInstance.remove();
+            }
+        };
+    }, []);
+
+    return <div id="map" className={'h-[250px] w-full'}></div>;
 }
